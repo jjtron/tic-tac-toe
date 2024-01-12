92f5f7ab8727a086237c9745f90dfb1e1be96613
jjtron <gpetron4@cfl.rr.com>
2024-01-12 11:30:56 -0500

    Initial tutorial work

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { StrictMode } from "react";
+import { useState } from 'react';
+function Square() {
+  const [val, setValue] = useState(null);
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {val}
+    </button>
+  );
+}
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
